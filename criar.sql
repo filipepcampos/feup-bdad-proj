ff29406b54f4d4ec43964f80fa3335298224152b
Filipe Campos <filipepcampos24@gmail.com>
2021-03-30 21:58:53 +0100

    Add strong date text checking constraints

--- criar.sql
+++ criar.sql
@@ -17,7 +17,11 @@ CREATE TABLE OfertaEmprego (
         posicao TEXT CONSTRAINT posicaoNotNull NOT NULL,
         informacao TEXT,
         salario REAL CONSTRAINT salarioPositivo CHECK(salario>=0),
-        dataCriacao TEXT CONSTRAINT dataCriacaoNotNull NOT NULL,
+        dataCriacao TEXT 
+            CONSTRAINT dataCriacaoNotNull NOT NULL
+            CONSTRAINT dataCriacaoFormat CHECK (dataCriacao LIKE "____-__-__")
+            CONSTRAINT dataCriacaoValue CHECK (dataCriacao IS strftime("%Y-%m-%d",dataCriacao)) -- Filter out "2021-01-99", for example
+            CONSTRAINT dataCriacaoNotFuture CHECK (dataCriacao <= strftime("%Y-%m-%d")),
         idEmpresa INTEGER CONSTRAINT idEmpresaNotNull NOT NULL, -- REFERENCES ...
         localizacaoId INT CONSTRAINT localizacaoFK REFERENCES Localizacao(id) ON DELETE SET NULL ON UPDATE SET NULL
 );
@@ -27,7 +31,11 @@ CREATE TABLE Candidatura(
         idOferta INTEGER CONSTRAINT idOfertaFK REFERENCES OfertaEmprego(id)
 				ON DELETE CASCADE ON UPDATE CASCADE,
         idJogador INTEGER, -- REFERENCES 
-        dataCandidatura TEXT CONSTRAINT dataCandidaturaNotNull NOT NULL, -- Check dataCandidatura >= Oferta.dataCriacao
+        dataCandidatura TEXT 
+            CONSTRAINT dataCandidaturaNotNull NOT NULL -- Check dataCandidatura >= Oferta.dataCriacao
+            CONSTRAINT dataCandidaturaFormat CHECK (dataCandidatura LIKE "____-__-__")
+            CONSTRAINT dataCandidaturaValue CHECK (dataCandidatura IS strftime("%Y-%m-%d",dataCandidatura)) -- Filter out "2021-01-99", for example
+            CONSTRAINT dataCandidaturaNotFuture CHECK (dataCandidatura <= strftime("%Y-%m-%d")),
         CONSTRAINT CandidaturaPK PRIMARY KEY(idOferta, idJogador)
 );
 
@@ -38,8 +46,16 @@ CREATE TABLE Competicao(
         descricao TEXT,
         dificuldadeMedia REAL -- CONSTRAINT dificuldadeMediaNotNull NOT NULL
                         CONSTRAINT dificuldadeMediaRange CHECK(dificuldadeMedia >= 0 AND dificuldadeMedia <= 10),
-        datetimeInicio TEXT CONSTRAINT datetimeInicioNotNull NOT NULL,
-        datetimeFim TEXT CONSTRAINT datetimeFimNotNull NOT NULL,
+        datetimeInicio TEXT 
+            CONSTRAINT datetimeInicioNotNull NOT NULL
+            CONSTRAINT datetimeInicioFormat CHECK (datetimeInicio LIKE "____-__-__ __:__:__")
+            CONSTRAINT datetimeInicioValue CHECK (datetimeInicio IS strftime("%Y-%m-%d %H:%m:%s", datetimeInicio))
+            CONSTRAINT datetimeInicioNotFuture CHECK (datetimeInicio <= strftime("%Y-%m-%d %H:%m:%s")),
+        datetimeFim TEXT 
+            CONSTRAINT datetimeFimNotNull NOT NULL
+            CONSTRAINT datetimeFimFormat CHECK (datetimeFim LIKE "____-__-__ __:__:__")
+            CONSTRAINT datetimeFimValue CHECK (datetimeFim IS strftime("%Y-%m-%d %H:%m:%s", datetimeFim))
+            CONSTRAINT datetimeFimNotFuture CHECK (datetimeFim <= strftime("%Y-%m-%d %H:%m:%s")),
         numParticipantes INTEGER, -- CONSTRAINT numParticipantesNotNull NOT NULL, -- Verificar isto
         premio TEXT, -- 'Descrição do prémio'
         CONSTRAINT integridadeTemporal CHECK(datetimeFim > datetimeInicio)
@@ -51,6 +67,10 @@ CREATE TABLE Participacao(
         idCompeticao INTEGER CONSTRAINT idCompeticaoFK REFERENCES Competicao(id)
 				ON DELETE CASCADE ON UPDATE CASCADE,
         dataInscricao TEXT CONSTRAINT dataInscricaoNotNull NOT NULL, -- Verificar se inscrição foi antes do inicio da competição
+            CONSTRAINT dataInscricaoNotNull NOT NULL
+            CONSTRAINT dataInscricaoFormat CHECK (dataInscricao LIKE "____-__-__ __:__:__")
+            CONSTRAINT dataInscricaoValue CHECK (dataInscricao IS strftime("%Y-%m-%d %H:%m:%s",dataInscricao)) 
+            CONSTRAINT dataInscricaoNotFuture CHECK (dataInscricao <= strftime("%Y-%m-%d %H:%m:%s"))
         posicao INTEGER CONSTRAINT posicaoNotNull NOT NULL,
         mudancaRating INTEGER,
         CONSTRAINT ParticipacaoPK PRIMARY KEY(idJogador, idCompeticao) 
@@ -83,17 +103,25 @@ CREATE TABLE Problema (
 
 DROP TABLE IF EXISTS Informacao;
 CREATE TABLE Informacao (
-        idCurso INTEGER REFERENCES Curso(id),
+    idCurso INTEGER REFERENCES Curso(id),
 	idJogador INTEGER, -- REFERENCES
 	dataInicio TEXT,
-	dataFim TEXT,
+        CONSTRAINT dataInicioNotNull NOT NULL
+        CONSTRAINT dataInicioFormat CHECK (dataInicio LIKE "____-__-__")
+        CONSTRAINT dataInicioValue CHECK (dataInicio IS strftime("%Y-%m-%d",dataInicio))
+        CONSTRAINT dataInicioNotFuture CHECK (dataInicio <= strftime("%Y-%m-%d")),
+	dataFim TEXT
+        CONSTRAINT dataFimNotNull NOT NULL
+        CONSTRAINT dataFimFormat CHECK (dataFim LIKE "____-__-__")
+        CONSTRAINT dataFimValue CHECK (dataFim IS strftime("%Y-%m-%d",dataFim))
+        CONSTRAINT dataFimNotFuture CHECK (dataFim <= strftime("%Y-%m-%d")),,
 	nota INTEGER,
 	PRIMARY KEY(idCurso, idJogador)
 );
 
 DROP TABLE IF EXISTS Aula;
 CREATE TABLE Aula (
-        id INTEGER PRIMARY KEY,
+    id INTEGER PRIMARY KEY,
 	texto TEXT,
 	videoURL TEXT,
 	idCurso INTEGER REFERENCES Curso(id)
@@ -101,13 +129,13 @@ CREATE TABLE Aula (
 
 DROP TABLE IF EXISTS ProblemaComSolucao;
 CREATE TABLE ProblemaComSolucao (
-        idProblema INTEGER PRIMARY KEY REFERENCES Problema(id),
-        solucao TEXT
+    idProblema INTEGER PRIMARY KEY REFERENCES Problema(id),
+    solucao TEXT
 );
 
 DROP TABLE IF EXISTS Desafio;
 CREATE TABLE Desafio (
-        id INTEGER PRIMARY KEY,
+    id INTEGER PRIMARY KEY,
 	idDesafiador INTEGER, -- REFERENCES
 	idDesafiado INTEGER, -- REFERENCES
 	descricao TEXT
@@ -115,21 +143,21 @@ CREATE TABLE Desafio (
 
 DROP TABLE IF EXISTS ProblemaCompeticao;
 CREATE TABLE ProblemaCompeticao (
-        idProblema REFERENCES Problema(id),
+    idProblema REFERENCES Problema(id),
 	idCompeticao REFERENCES Competicao(id),
 	PRIMARY KEY(idProblema, idCompeticao)
 );
 
 DROP TABLE IF EXISTS ProblemaAula;
 CREATE TABLE ProblemaAula (
-        idProblema REFERENCES Problema(id),
+    idProblema REFERENCES Problema(id),
 	idAula REFERENCES Aula(id),
 	PRIMARY KEY(idProblema, idAula)
 );
 
 DROP TABLE IF EXISTS ProblemaDesafio;
 CREATE TABLE ProblemaDesafio (
-        idProblema REFERENCES Problema(id),
+    idProblema REFERENCES Problema(id),
 	idDesafio REFERENCES Desafio(id),
 	PRIMARY KEY(idProblema, idDesafio)
 );
