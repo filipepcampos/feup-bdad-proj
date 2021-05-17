08cd9e6f3ff0b3ce3f89205f953ef8486caeefc8
Filipe Campos <filipepcampos24@gmail.com>
2021-05-17 21:52:42 +0100

    Another one bites the dust :musical_note:

---
+++ tmp7.sql
@@ -0,0 +1,9 @@
+-- Mostrar numero de ofertas para cada (pais, empresa)
+
+SELECT localPais as Pais, nome, count(OfertaEmprego.id) as numOfertas 
+FROM Localizacao
+    JOIN OfertaEmprego 
+        ON (localPais=pais AND localCidade=cidade AND localEndereco=endereco)
+    JOIN Utilizador
+        ON (idEmpresa = Utilizador.id)
+GROUP BY Pais, nome;
