e6bdbeb1dbe4e64c140be217f250dcf53e4c0a0f
Vasco52 <64026324+Vasco52@users.noreply.github.com>
2021-05-17 23:44:58 +0100

    Progress on new query

---
+++ tmp4.sql
@@ -0,0 +1,16 @@
+-- Mostrar lista de Empresas ordenadas pela sua procura (Número total de candidaturas a OfertasEmprego criadas por essa empresa)
+
+.mode columns
+.headers on
+.nullvalue NULL
+
+
+SELECT Empresa.id as Empresa, website, count(OfertaEmprego.idEmpresa) as numCandidaturas
+FROM Empresa
+	JOIN OfertaEmprego
+		ON (Empresa.id = idEmpresa)
+	JOIN Candidatura
+		ON (OfertaEmprego.id = idOferta)
+	ORDER BY numCandidaturas DESC;
+
+// TODO : THIS IS NOT WORKING
