89f87801206300c1208303d36cfeb8ddfe100ccf
Filipe Campos <filipepcampos24@gmail.com>
2021-05-17 11:33:40 +0100

    I've created a monster :beers: :boom: :poop:

--- tmp4.sql
+++ tmp4.sql
@@ -1,37 +1,49 @@
 -- Selecionar número de problemas resolvidos por cada jogador (via competição, desafio ou aula). Nota: Problemas com solução de uma aula não contam para este valor!
 
-SELECT * FROM
-(
-    -- [COUNT PROBLEMS FROM Competicao]
-    SELECT idJogador, count(DISTINCT idProblema) as numProblemasCompeticao
-    FROM Competicao 
-        JOIN (SELECT idJogador, idCompeticao as id FROM Participacao) USING(id)
-        JOIN (SELECT idProblema, idCompeticao as id FROM ProblemaCompeticao) USING(id)
-    GROUP BY idJogador
-)
-JOIN
-(
-    -- [ COUNT PROBLEMS FROM Desafio ]
-    SELECT idJogador, count(DISTINCT idProblema) as numProblemasDesafio
-    FROM ProblemaDesafio 
-        JOIN
-            (SELECT id, idDesafiado as idJogador FROM Desafio
-            UNION
-            SELECT id, idDesafiador as idJogador FROM Desafio)
-        WHERE idDesafio = id
-    GROUP BY idJogador
-) USING(idJogador);
+SELECT id
+    ifnull(numProblemasCompeticao, 0) as numProblemasCompeticao,
+    ifnull(numProblemasDesafio, 0) as numProblemasDesafio,
+    ifnull(numProblemasEmAula, 0) as numProblemasEmAula
+FROM
+    Jogador
+    LEFT JOIN
+    (
+        -- Contar problemas de competições
+        SELECT idJogador, count(DISTINCT idProblema) as numProblemasCompeticao
+        FROM Competicao 
+            JOIN (SELECT idJogador, idCompeticao as id FROM Participacao) USING(id)
+            JOIN (SELECT idProblema, idCompeticao as id FROM ProblemaCompeticao) USING(id)
+        GROUP BY idJogador
+    ) AS T1 ON Jogador.id = T1.idJogador
 
+    LEFT JOIN
+    (
+        -- Contar problemas de desafios
+        SELECT idJogador, count(DISTINCT idProblema) as numProblemasDesafio
+        FROM ProblemaDesafio 
+            JOIN
+                (SELECT id, idDesafiado as idJogador FROM Desafio
+                UNION
+                SELECT id, idDesafiador as idJogador FROM Desafio)
+            WHERE idDesafio = id
+        GROUP BY idJogador
+    ) AS T2 ON Jogador.id = T2.idJogador
 
---SELECT idJogador, sum(numProblemasCurso) as numProblemasEmAula
---FROM Jogador 
-    --JOIN
-      --  (SELECT idJogador, idCurso FROM Informacao) 
-       -- ON Jogador.id = idJogador
-    --JOIN (
-        --SELECT idCurso, count(DISTINCT idProblema) as numProblemasCurso
-          --  FROM Aula 
-           --     JOIN ProblemaAula ON Aula.id = idAula 
-        --GROUP BY idCurso
-    --) USING (idCurso)
---GROUP BY idJogador;
+    LEFT JOIN
+    (
+        -- Contar problemas provenientes de aulas (Problemas com solução não contam)
+        SELECT idJogador, sum(numProblemasCurso) as numProblemasEmAula
+        FROM Jogador 
+            JOIN
+                (SELECT idJogador, idCurso FROM Informacao) 
+            ON Jogador.id = idJogador
+            JOIN (
+                SELECT idCurso, count(DISTINCT idProblema) as numProblemasCurso
+                        FROM Aula 
+                    JOIN ProblemaAula ON Aula.id = idAula 
+                GROUP BY idCurso
+            ) USING (idCurso)
+        GROUP BY idJogador
+    ) AS T3 ON Jogador.id = T3.idJogador
+;
+
