c9dd58b14735fb2bc9e47dc7287b1d808bbaa686
Filipe Campos <filipepcampos24@gmail.com>
2021-05-18 15:13:15 +0100

    tmp10.sql better formatting

--- tmp10.sql
+++ tmp10.sql
@@ -1,5 +1,7 @@
 -- Mostrar mensagens num formato facil de ler, isto é, mostrar o username dos utilizadores em vez do id e mostrar a data formatada em vez do valor unixepoch
 
 SELECT usernameRemetente, usernameDestinatario, texto, datetime(Mensagem.datetime, 'unixepoch') as data FROM Mensagem
-    JOIN (SELECT id, username as usernameRemetente FROM Utilizador) AS T1 ON (T1.id = Mensagem.idUtilizadorRemetente)
-    JOIN (SELECT id, username as usernameDestinatario FROM Utilizador) AS T2 ON (T2.id = Mensagem.idUtilizadorDestinatario);
+    JOIN (SELECT id, username as usernameRemetente FROM Utilizador) AS T1 
+        ON (T1.id = Mensagem.idUtilizadorRemetente)
+    JOIN (SELECT id, username as usernameDestinatario FROM Utilizador) AS T2 
+        ON (T2.id = Mensagem.idUtilizadorDestinatario);
