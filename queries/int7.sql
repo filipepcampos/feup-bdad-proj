9f1b18134c905b60aba2e3b06fd3a2962502c81a
Filipe Campos <filipepcampos24@gmail.com>
2021-05-22 14:27:30 +0100

    Another query ready to go

---
+++ queries/int7.sql
@@ -0,0 +1,25 @@
+-- [Descrição]
+-- Qual o username e nota média de todos os jogadores que concluíram todos os cursos disponíveis
+-- Adicionalmente, também se quer saber qual dos cursos foi mais recentemente terminado por cada um desses jogadores 
+-- (Em caso de empate escolher um curso aleatório)
+
+DROP VIEW IF EXISTS cursoMaisRecentePorJogador;
+CREATE VIEW cursoMaisRecentePorJogador AS
+SELECT idJogador, idCurso, titulo, nivel FROM Curso JOIN(
+    SELECT idJogador, idCurso FROM Informacao
+    GROUP BY idJogador
+    HAVING dataFim = max(dataFim)
+) ON (Curso.id = idCurso);
+
+SELECT id, username, notaMedia, idCurso, titulo, nivel
+FROM Utilizador
+    JOIN (
+        SELECT idJogador, count(DISTINCT idCurso) as numCursosCompletados, avg(nota) as notaMedia
+        FROM Informacao
+        WHERE nota >= 9.5
+        GROUP BY idJogador
+    ) ON (id=idJogador)
+    JOIN (
+        SELECT * from cursoMaisRecentePorJogador
+    ) USING(idJogador)
+WHERE numCursosCompletados = (SELECT count(id) FROM Curso);
